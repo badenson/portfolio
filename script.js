26e1cf0d42ea4a972381802c00734a5f71250a04
axcessit <axcessit@gmail.com>
2024-10-30 20:45:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-// Some code thanks to @chrisgannon
-
-var select = function(s) {
-  return document.querySelector(s);
-}
-
-function randomBetween(min,max)
-{
-    var number = Math.floor(Math.random()*(max-min+1)+min);
-  
-    if ( number !== 0 ){
-      return number;
-    }else {
-      return 0.5;
+async function fetchVisitorCount() {
+    try {
+        const response = await fetch('https://your-api-url.com/visitorcount');
+        const data = await response.json();
+        document.getElementById('visitor-count').innerText = data.count;
+    } catch (error) {
+        console.error('Error fetching visitor count:', error);
     }
 }
 
-var tl = new TimelineMax();
-
-for(var i = 0; i < 20; i++){
-
-  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-    y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
-    repeat:-1,
-    repeatDelay:randomBetween(0.2, 0.5),
-    yoyo:true,
-    ease:Elastic.easeOut.config(1, 0.5)
-  })
-
-  tl.add(t, (i+1)/0.6)
-}
-
-tl.seek(50);
+fetchVisitorCount();
