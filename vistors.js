4e752b028ecf02e1572a167a003a978daa935fd6
axcessit <axcessit@gmail.com>
2024-11-07 13:48:11 -0500

    Update vistors.js

--- vistors.js
+++ vistors.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const response = await fetch("https://ojw4oegq5l.execute-api.us-east-1.amazonaws.com/visitorcount");
+        const response = await fetch("https://q8m0mravtc.execute-api.us-east-1.amazonaws.com/visitorcount");
         const data = await response.json();
         document.getElementById("count").innerText = data.count;
     } catch (error) {
