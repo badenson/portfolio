56b152e9d7244c9fe11bb26419090c999ac36b17
axcessit <axcessit@gmail.com>
2024-11-24 11:18:39 -0500

    Update vistors.js

--- vistors.js
+++ vistors.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", async () => {
+const counter = document.getElementById("counter");
+async function updateCounter() {
     try {
-        const response = await fetch("https://6tz57xjw2vvmlup2a4ea2dp6ga0uyvqc.lambda-url.us-east-1.on.aws/");
-        const data = await response.json();
-        document.getElementById("count").innerText = data.count;
+      let response = await fetch("https://6tz57xjw2vvmlup2a4ea2dp6ga0uyvqc.lambda-url.us-east-1.on.aws/",);
+      let data = await response.json();
+      console.log("Response Data:", data);
+      counter.innerHTML = `Views: ${data.count}`;
     } catch (error) {
-        console.error("Error fetching visitor count:", error);
+      console.error("Error fetching data:", error.message);
+      counter.innerHTML = "Error loading counter";
     }
-});
+  }
+  updateCounter();
