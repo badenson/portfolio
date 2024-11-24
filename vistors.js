9bcbbf2b2c5795648356a66b93c714ede914adb0
axcessit <axcessit@gmail.com>
2024-11-24 10:51:58 -0500

    Update vistors.js

--- vistors.js
+++ vistors.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const response = await fetch("https://gn4ytme5f37wzlbja37xubhxya0dlakw.lambda-url.us-east-1.on.aws/");
+        const response = await fetch("https://6tz57xjw2vvmlup2a4ea2dp6ga0uyvqc.lambda-url.us-east-1.on.aws/");
         const data = await response.json();
         document.getElementById("count").innerText = data.count;
     } catch (error) {
