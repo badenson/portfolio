1b39fa707a29ae1ab24b758cfe3f36fe485741ed
axcessit <axcessit@gmail.com>
2024-11-24 11:46:10 -0500

    Add files via upload

--- vistors.js
+++ vistors.js
@@ -1,13 +1,9 @@
-const counter = document.getElementById("counter");
-async function updateCounter() {
+document.addEventListener("DOMContentLoaded", async () => {
     try {
-      let response = await fetch("https://6tz57xjw2vvmlup2a4ea2dp6ga0uyvqc.lambda-url.us-east-1.on.aws/",);
-      let data = await response.json();
-      console.log("Response Data:", data);
-      counter.innerHTML = `Views: ${data.count}`;
+        const response = await fetch("https://gn4ytme5f37wzlbja37xubhxya0dlakw.lambda-url.us-east-1.on.aws/");
+        const data = await response.json();
+        document.getElementById("count").innerText = data.count;
     } catch (error) {
-      console.error("Error fetching data:", error.message);
-      counter.innerHTML = "Error loading counter";
+        console.error("Error fetching visitor count:", error);
     }
-  }
-  updateCounter();
+});
