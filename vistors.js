5fbea963093bf009fbd5aa9553b946a88ac41ebf
axcessit <axcessit@gmail.com>
2024-11-26 10:06:32 -0500

    visitor.js update

--- vistors.js
+++ vistors.js
@@ -4,7 +4,8 @@ async function updateCounter() {
       let response = await fetch("https://.lambda-url.us-east-1.on.aws/",);
       let data = await response.json();
       console.log("Response Data:", data);
-      counter.innerHTML = `Views: ${data.count}`;
+      //counter.innerHTML = `Views: ${data.count}`;
+      counter.innerHTML = `Views: 123`;
     } catch (error) {
       console.error("Error fetching data:", error.message);
       counter.innerHTML = "Error loading counter";
